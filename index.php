3edc0038d6ecadef82de642323bb9027f70f0e0a
IgorAngelotti <lanzaigor@live.com>
2024-04-03 12:34:33 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// cabeçalho com metadados
+include_once './layout/head.php';
+
+// cabeçalho com menu
+include_once './layout/header.php';
+
+// conteudo
+include_once './layout/main.php';
+
+// scripts
+include_once './layout/scripts.php';
+
+// footer
+include_once './layout/footer.php';
+
+
---
+++ layout/main.php
@@ -0,0 +1,18 @@
+<!-- main.php -->
+<main>
+<div class="px-4 pt-5 my-5 text-center border-bottom">
+    <h1 class="display-4 fw-bold text-body-emphasis">Meu site em PHP</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Site básico usando o php para carregar as diferentes partes da página, sendo o head, header, main (este conteúdo) e o footer. Usando o bootstrap para dar um estilo bonito e de forma rápida.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Saiba mais</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Conheça o PHP</button>
+      </div>
+    </div>
+    <div class="overflow-hidden" style="max-height: 30vh;">
+      <div class="container px-5">
+        <img src="https://getbootstrap.com/docs/5.3/examples/heroes/bootstrap-docs.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+      </div>
+    </div>
+  </div>
+</main>
